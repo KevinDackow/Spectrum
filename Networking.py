d20f83efa9051636d5e576d9a6040163780d1eda
KevinDackow <Kevin Dackow>
2018-01-27 23:33:05 -0500

    bug fixes

--- Networking.py
+++ Networking.py
@@ -3,7 +3,7 @@ import json
 import requests
 from datetime import datetime, date, time, timedelta
 ################################### API Definitions ############################################
-hours_to_refresh = 24
+hours_to_refresh = 4
 
 first_groupAPIs = "https://newsapi.org/v2/everything?sources=abc-news,al-jazeera-english,associated-press,bbc-news,bloomberg,breitbart-news,business-insider,cbs-news,cnn,daily-mail,entertainment-weekly,espn,financial-post,financial-times,fortune,fox-news,hacker-news,independent,medical-news-today,msnbc&from={0}&pageSize=100&apiKey=c98f2be3bafc441bb170235cba31516b".format((datetime.utcnow() - timedelta(hours=hours_to_refresh)).isoformat())
 
